56dc2993a2f4c2eb2815f32dbf661ca7c20a51d3
wendellxavier <wendell_xw20@hotmail.com>
2024-05-04 09:52:52 -0300

    "arrumando  a pagina e as views"

--- receitas/views.py
+++ receitas/views.py
@@ -9,10 +9,10 @@ def home(request):
 
 
 def category(request, category_id):
-    receitas = get_list_or_404(Receita, category__id=category_id, is_published=True)
+    receitas = get_list_or_404(Receita.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
     return render(request, 'category.html', context={'receitas': receitas, 'title': f'{receitas.first().category.name} - Categoria'})
 
 
 def receitas_detalhe(request, id_receitas):
-    receitas = Receita.objects.get(id=id_receitas)
-    return render(request, 'receitas.html', context={'receitas': receitas,'is_detail_page': True})
+    receita = Receita.objects.get(id=id_receitas)
+    return render(request, 'receitas.html', context={'receita': receita,'is_detail_page': True})
