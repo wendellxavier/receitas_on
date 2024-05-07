37cac64fb3d841fbd1e01ce32f41b1afb47797fe
wendellxavier <wendell_xw20@hotmail.com>
2024-05-07 09:50:41 -0300

    "começando a testar"

--- receitas/views.py
+++ receitas/views.py
@@ -4,15 +4,15 @@ from .models import Receita
 
 
 def home(request):
-    receitas = Receita.objects.filter(is_published=True,).order_by('-id')
+    receitas = get_list_or_404(Receita.objects.filter(is_published=True).order_by('-id'))
     return render(request, 'home.html', context={'receitas': receitas})
 
 
 def category(request, category_id):
     receitas = get_list_or_404(Receita.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
-    return render(request, 'category.html', context={'receitas': receitas, 'title': f'{receitas.first().category.name} - Categoria'})
+    return render(request, 'category.html', context={'receitas': receitas, 'title': f'{receitas[0].category.name} - Categoria'})
 
 
 def receitas_detalhe(request, id_receitas):
-    receita = Receita.objects.get(id=id_receitas)
+    receita = get_object_or_404(Receita, id=id_receitas, is_published=True)
     return render(request, 'receitas.html', context={'receita': receita,'is_detail_page': True})
