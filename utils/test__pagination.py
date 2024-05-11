0e549ef3855514d2595528aac1e46c86aec1954f
wendellxavier <wendell_xw20@hotmail.com>
2024-05-11 10:06:16 -0300

    "cofiguração de paginas"

--- utils/test__pagination.py
+++ utils/test__pagination.py
@@ -3,5 +3,12 @@ from utils.pagination import make_pagination_range
 
 class PaginationTest(TestCase):
     def test_make_pagination_range_returns_a_pagination_range(self):
-        pagination = make_pagination_range(page_range=list(range(1, 30)), qty_paginas=4, current_page=1,)
+        pagination = make_pagination_range(page_range=list(range(1, 21)), qty_pages=4, current_page=1,)
+        self.assertEqual([1, 2, 3, 4], pagination)
+        
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
+        pagination = make_pagination_range(page_range=list(range(1, 21)), qty_pages=4, current_page=1,)
+        self.assertEqual([1, 2, 3, 4], pagination)
+        
+        pagination = make_pagination_range(page_range=list(range(1, 21)), qty_pages=4, current_page=2,)
         self.assertEqual([1, 2, 3, 4], pagination)
